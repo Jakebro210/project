783b4698ab90a4c1bd2b03fa1dce33db6fd1d123
andy-nunez <moe316@my.utsa.edu>
2020-11-01 13:26:55 -0600

    while

--- while.sh
+++ while.sh
@@ -1,3 +1,5 @@
+#The purpose of this block was to counter up by an increment of 1
+#I thought it was interesting that we had to use the "$" in front of counter. I was not familiar with using this prior to Project 2.
 #!/bin/bash
 
 counter=0
